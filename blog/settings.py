b5515385899c9803478241a2e70610702087b392
Philip Okiokio <philipokiokio@gmail.com>
2021-04-26 18:12:00 +0100

    deployment

--- blog/settings.py
+++ blog/settings.py
@@ -16,12 +16,12 @@ import dotenv
 
 
 
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
 dotenv_file = Path.joinpath(BASE_DIR, '.env')
 if os.path.isfile(dotenv_file):
     dotenv.load_dotenv(dotenv_file)
-
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
