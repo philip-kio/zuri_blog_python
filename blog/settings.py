a3b96e8b8609a3fe08b5bd3ec98d6cef5e00176c
Philip Okiokio <philipokiokio@gmail.com>
2021-04-26 18:16:23 +0100

    deployment

--- blog/settings.py
+++ blog/settings.py
@@ -28,7 +28,7 @@ if os.path.isfile(dotenv_file):
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv['SECRET_KEY']
+SECRET_KEY = os.environ('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False #os.getenv('DEBUG')
